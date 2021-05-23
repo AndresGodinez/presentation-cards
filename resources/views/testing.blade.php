8583656ec3b02117e5f333cf2b1a3d5c3f049268
AGodinez <ing.a.godinez@gmail.com>
2021-05-23 11:22:40 -0500

    First commit

---
+++ resources/views/testing.blade.php
@@ -0,0 +1,37 @@
+@extends('Layouts/app')
+@section('content')
+    <div class="row">
+        <div class="container">
+            <div class="col-md-5">
+
+            </div>
+            <div class="col-md-7">
+                <div class="row">
+                    <h2>Title</h2>
+                    <h4>SubTitle</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores officia provident sit suscipit voluptate? Amet, assumenda cumque dolorum, excepturi harum id inventore itaque maxime perspiciatis repudiandae sunt suscipit voluptatibus voluptatum?</p>
+                </div>
+                <div class="row">
+                    <h3>Objectives</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores officia provident sit suscipit voluptate? Amet, assumenda cumque dolorum, excepturi harum id inventore itaque maxime perspiciatis repudiandae sunt suscipit voluptatibus voluptatum?</p>
+                </div>
+                <div class="row">
+                    <h3>Web</h3>
+                    <p>www.carvaz.com</p>
+                </div>
+
+                <div class="row">
+                    <a href="#" class="btn btn-primary">Download Resume</a>
+                </div>
+                <div class="row">
+                    <h6>Social media</h6>
+                    i
+                    i
+                    i
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
