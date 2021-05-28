b5f394061fa8f87c2c1987cc2b71d50dd8512b8c
AGodinez <ing.a.godinez@gmail.com>
2021-05-28 11:23:58 -0500

    Install tailwind

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -1,10 +1,42 @@
 @extends('Layouts/app')
 @section('content')
+
+    <div class="container mx-auto px-4">
+        <figure class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+            <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://picsum.photos/id/237/200/300" alt="" width="384" height="512">
+            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+                <blockquote>
+                    <p class="text-lg font-semibold">
+                        “Tailwind CSS is the only framework that I've seen scale
+                        on large teams. It’s easy to customize, adapts to any design,
+                        and the build size is tiny.”
+                    </p>
+                </blockquote>
+                <figcaption class="font-medium">
+                    <div class="text-cyan-600">
+                        Sarah Dayan
+                    </div>
+                    <div class="text-gray-500">
+                        Staff Engineer, Algolia
+                    </div>
+                </figcaption>
+            </div>
+        </figure>
+
+    </div>
+<div class="h-64 grid gird-rows-2 grid-flow-col gap-4">
+    <div>
+        <img src="https://picsum.photos/id/237/200/300" alt="test">
+    </div>
+    <div>
+        2
+    </div>
+    <div>3</div>
+</div>
+
     <div class="row">
         <div class="container">
-            <div class="col-md-5">
 
-            </div>
             <div class="col-md-7">
                 <div class="row">
                     <h2>Title</h2>
